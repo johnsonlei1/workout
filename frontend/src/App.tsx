8257753bb1b3d4c69b94baa0a3b8806dced37da4
johnson.lei05 <johnson.lei05@gmail.com>
2025-03-30 19:25:54 -0400

    appwrite yo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-
+import "./App.css";
+import { Client } from "appwrite";
+const client = new Client();
+client.setProject("67e9d1c10001b5c3836e");
 function App() {
   const [data, setData] = useState(null);
 
@@ -11,6 +14,9 @@ function App() {
 
   return (
     <div>
+      <header className="header">
+        <h1 className="title">Workout Logger</h1>
+      </header>
       <h1>{data ? data : "Loading..."}</h1>
     </div>
   );
