64e9a3f1582aaba389777c52c09892e43ce1b768
houricky <richardhouston18@gmail.com>
2025-03-31 21:00:37 -0400

    navbar changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../services/auth";
+import { login } from "../components/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
