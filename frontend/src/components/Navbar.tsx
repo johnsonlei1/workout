e6aa4dcc5abffa88c54905bdcca7b30a186a4bee
houricky <richardhouston18@gmail.com>
2025-03-31 21:55:49 -0400

    redid logout
    logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,14 @@
 import { Link } from "react-router-dom";
 import "../App.css";
+import {logout} from "./auth.tsx"
+import {Client, Account} from "appwrite"
+
+const client = new Client();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("67e9d1c10001b5c3836e");
+
+const account = new Account(client);
 
 const Navbar = () => {
   return (
@@ -13,6 +22,9 @@ const Navbar = () => {
             </Link>
             <Link to="/Dashboard" className="link">
               Dashboard
+            </Link>
+            <Link to="/Login" className="link" onClick={logout}> 
+            Logout
             </Link>
           </div>
         </div>
