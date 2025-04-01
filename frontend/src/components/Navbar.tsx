90fb2dfaaead8174d9037bdc460ca2fb272145f8
houricky <richardhouston18@gmail.com>
2025-03-31 21:21:06 -0400

    nav bar done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,14 +2,17 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 const Navbar = () => {
-
   return (
     <header>
-        <div className="header">
-           <header className="title">Workout Logger</header>
+      <div className="header">
+        <div className="title-with-links">
+          <header className="title">Workout Logger</header>
+          <div className="nav-links">
             <Link to="/Community" className="link">To Community</Link>
             <Link to="/Dashboard" className="link">To Dashboard</Link>
+          </div>
         </div>
+      </div>
     </header>
   );
 };
